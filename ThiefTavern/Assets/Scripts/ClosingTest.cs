49fd113f67bee1764ab275ef9a38ea2d923435c1
FatAlien <vladislav.chigrinov@gmail.com>
2021-04-21 11:23:00 +0300

    Добавил сцену воров и несколько шрифтов

---
+++ ThiefTavern/Assets/Scripts/ClosingTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class ClosingTest : MonoBehaviour
+{
+    public Action Closer;
+
+    private void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Closer += () => Debug.Log(i);
+        }
+
+        Closer();
+    }
+}
