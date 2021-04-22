7c229340c8de0b3dad4843af3f02e814e24f52b4
koogel <koogelpotato@gmail.com>
2021-04-22 18:13:16 +0300

    немного перенес подвал

---
+++ ThiefTavern/Assets/Scripts/podva.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class podval_controler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+         public Transform ruputacia;
+         public Transform resursi;
+         
+    
+    public void OnClick_Podval_reputachia_on() 
+    {
+        ruputacia.gameObject.SetActive(true);
+        
+    }
+    public void OnClick_Podval_reputachia_off() 
+    {
+        ruputacia.gameObject.SetActive(false);
+       
+    }
+    public void OnClick_Podval_resursi_on() 
+    {
+        resursi.gameObject.SetActive(true);
+       
+    }
+    public void OnClick_Podval_resursi_off()
+    {
+        resursi.gameObject.SetActive(false);
+       
+    }
+   
+
+
+    
+}
