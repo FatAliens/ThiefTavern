4eaf7df37872d8d93713d1c918dee42f05cfec06
FatAlien <vladislav.chigrinov@gmail.com>
2021-04-23 13:42:48 +0300

    Add tooltip to thiefs skills

---
+++ ThiefTavern/Assets/TooltipController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(CanvasGroup), typeof(RectTransform))]
+public class TooltipController : MonoBehaviour
+{
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+    }
+
+    private CanvasGroup canvasGroup;
+    public void OnPointerEnter()
+    {
+        canvasGroup.DOFade(1, 0.2f);
+    }
+
+    public void OnPointerExit()
+    {
+        canvasGroup.DOFade(0, 0.4f);
+    }
+}
